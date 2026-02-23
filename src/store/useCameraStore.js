03480c9c5f6dc4f8178d59f9d76dfe49be8341b7
Vuong Ho <hochivuong2002@gmail.com>
2026-02-22 21:27:30 -0500

    Start from clean slate

--- src/store/useCameraStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useCameraStore = create((set) => ({
-  zoom: 1,
-  setZoom: (zoom) => set({ zoom }),
-}));
--- src/store/useResponsiveStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useResponsiveStore = create(() => ({
-  isMobile: window.innerWidth < 764,
-}));
-
-window.addEventListener("resize", () => {
-  useResponsiveStore.setState({ isMobile: window.innerWidth < 764 });
-});
